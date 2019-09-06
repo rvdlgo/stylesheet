876aec7add7cfeadb0dafcef496f68bf095ccf5c
Robert van de Linde <robert.van.de.linde@gmail.com>
2019-09-06 17:39:44 +0200

    redesign 05-09
    redesign 05-09

--- js/amc-functions.js
+++ js/amc-functions.js
@@ -7,16 +7,19 @@ $(document).ready(function() {
 	        return false;
 		}
 	}
-	var navItems = $('nav .item a');
+	/* aanpassingen BO */
+	if ($(".headerright .searchToggle").length){ //check if available
+		$(".headerright .searchToggle").click(function() { // Click event
+			$("header .search-default").toggle(); // Toggle between display block and none
+		});
+	}
+	/* /aanpassingen BO */
+	var navItems = $('#navigation a');
 	if (navItems.length) {
 		navItems.each(function(index) {
 			// skip the homepage
 			if (index > 0) {
-				$(this).click(function(e){
-                    console.log(e);
-                    debugger;
-                    toggleNavFunction(index)
-                });
+				$(this).click(toggleNavFunction(index));
 			}
 		});
 	}
@@ -60,7 +63,7 @@ function TabsActivate(rootnode, selectedindex) {
         var kid;
 
         for (i=0; i<len; i++) {
-            kid = kids[i];
+            kid =  /*F5_*/  F5_Deflate_index( /*_5F##*/ kids /*F5_*/ , /*_5F#[#*/ i /*F5_*/ ) /*_5F#]#*/ ;
             if (kid.nodeName.toLowerCase() === "div") {
                 result.push(kid);
             }
@@ -72,8 +75,8 @@ function TabsActivate(rootnode, selectedindex) {
         var classname;
         var i;
         for (i=0; i < panes.length; i++) {
-            classname = panes[i].className.replace("tabsactive","tabsinactive");
-            panes[i].className = classname.replace("tabsinactive",
+            classname =  /*F5_*/  F5_Invoke_replace( F5_Deflate_index( /*_5F##*/ panes /*F5_*/ , /*_5F#[#*/ i /*F5_*/ ) /*_5F#]#*/ .className /*F5_*/ , /*_5F#.replace(#*/ "tabsactive","tabsinactive");
+             /*F5_*/  F5_Deflate_index( /*_5F##*/ panes /*F5_*/ , /*_5F#[#*/ i /*F5_*/ ) /*_5F#]#*/ .className =  /*F5_*/  F5_Invoke_replace( /*_5F##*/ classname /*F5_*/ , /*_5F#.replace(#*/ "tabsinactive",
                 i === selectedindex ? "tabsactive" : "tabsinactive");
         }
     }
@@ -83,9 +86,144 @@ function TabsActivate(rootnode, selectedindex) {
     activatePane(findDivs(rootdivs[1]), selectedindex);
     return false;
 }
- $(function() {
+  $(function() {
     $( ".accordion" ).accordion({
       heightStyle: "content"
     });
   });
 
+
+/*
+function fillResult(mode, data) {
+	//alert('ping');
+	var moreResultsMode = false;
+	if ($('div#pagingblock a.moreresults').length > 0) {
+		moreResultsMode = true;
+	}
+    if (mode != 'moreresults') {
+        $('#searchresult').html("");
+    }
+    var from = parseInt(fromMaster);
+    var nrOfItemsPerPage = parseInt(numberOfItemsPerPage);
+	var itemOrder = data.itemOrder.split(",");
+
+    var until = from + nrOfItemsPerPage;
+    if (data.results.length < nrOfItemsPerPage) {
+        until = from + data.results.length;
+    }
+
+    newData = '<div class="search-result-count">';
+
+    if (data.results.length == 0) {
+        newData += noResultsFound;
+    } else {
+        newData += resultsFrom + ' ' + (from + 1) + ' ' + resultsUntil + ' ' + until;
+    }
+
+    newData += '</div>'
+    newData += '<div class="search-wrapper">';
+    newData += '<div id="searchresultbox"><ol>';
+
+    $.each(data.results, function(i,r){
+        var title = r.title;
+        if (title == undefined || title == '') {
+            title = r.location;
+        }
+        newData += '<li>';
+		var rendersoftware = r.rendersoftware;
+		if(rendersoftware == 'true'){
+			index = from + i + 1;
+			newData += '<h3>' + title;
+			if (r.version != null && r.version.length > 0) {
+				newData += ' (';
+				$.each(r.version, function(j,s) {
+					newData += s;
+				})
+				newData += ')';
+			}
+			newData += ' | <a id="anchor_' + index + '" href="#' + index + '" onclick="searchResultSelected(\'' + r.location + '\');" title="' + title + '">aanvragen</a></h3>';
+			newData += '<div class="summary">';
+			if (r.details != null && r.details.length > 0) {
+				$.each(r.details, function(j,s) {
+					newData += s + '<br/>';
+				})
+			}
+			newData += '[Software]</div>';
+
+		}else{
+			for (itemIndex = 0; itemIndex<itemOrder.length; itemIndex++) {
+				itemProperty = itemOrder[itemIndex];
+
+				if (itemProperty == 'title') {
+				  index = from + i + 1;
+					newData += '<h3><a id="anchor_' + index + '" href="#' + index + '" onclick="searchResultSelected(\'' + r.location + '\');" title="' + title + '">' + title + '</a></h3>';
+				} else if (itemProperty == 'summary') {
+					newData += '<div class="summary">';
+
+					if (r.snippets != null && r.snippets.length > 0) {
+						$.each(r.snippets, function(j,s) {
+							newData += s.snippet;
+							if (!(r.snippets.length == (j + 1))) {
+								newData += ' ... ';
+							}
+						})
+					} else {
+						newData += r.summary;
+					}
+					newData += '</div>';
+				} else if (itemProperty == 'meta') {
+					var extra = '';
+					if (showPublicationDate == 'true' && r.date != '') {
+						extra += '<span class="publicationDateLabel">' + published + '</span>';
+						extra += '<span class="publicationDate">' + r.date + '</span>';
+					}
+					if (showContentType == 'true' && r.category != '') {
+						if (showPublicationDate == 'true' && r.date != '') {
+							extra += '<span class="splitter">' + splitter + '</span>';
+						}
+						extra += '<span class="category">' + r.category + '</span>';
+					}
+					extra += '<span class="url">' + r.location + '</span>';
+
+					// Cater for overrule plugins here
+					if (typeof postMeta == 'function') {
+						extra += postMeta(r, extra == '');
+					}
+
+
+					newData += '<div class="meta">' + extra + '</div>';
+				} else if (itemProperty == 'leadimage') {
+					if (r.leadimage != '') {
+						newData += '<div class="leadimage"><img src="' + r.leadimage + '"/></div>';
+					}
+				} else if (itemProperty == 'count') {
+					newData += '<span class="count">' + r.count + '</span>';
+				}
+			}
+		}
+    newData += '</li>';
+    });
+
+    newData += '</ol></div></div><div class="clearer"><!--  --></div>';
+
+    $('#searchresult').append(newData);
+
+    // add the paging
+    var pagingHtml;
+	if (moreResultsMode) {
+		pagingHtml = createPagingBlock('moreresults', data);
+	} else {
+		pagingHtml = createPagingBlock(mode, data);
+	}
+	if (pagingHtml != "") {
+		$('#pagingblock').html("");
+		$('#pagingblock').append(pagingHtml);
+	} else {
+		$('#pagingblock').remove();
+	}
+	$('#facetinfo').html('');
+	$('div .searchresult-tabbedfacet').html('');
+	fillValues(data);
+}
+
+*/
